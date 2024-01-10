0c8c1279a957128a410290447e30af939abc578e
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-01-11 00:27:07 +0900

    feat: add detail

--- DatingCounselor.py
+++ DatingCounselor.py
@@ -1,17 +1,23 @@
 import streamlit as st
 from transformers import pipeline
 from openai import OpenAI
-import openai
 import os
+from PIL import Image
 
-#key
-os.environ["OPENAI_API_KEY"] = ""
 
-#client 불러오기
+os.environ["OPENAI_API_KEY"] = "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"
+img = Image.open('loveCon.png')
 client = OpenAI()
 
-st.title("나는야 연애고수~!! 나에게 상담을 맡겨봐 ><~!!")
-Prompt = st.text_input("너의 현재 연애고민을 알려줘!ㅇ!")
+st.title("러뷰!~ 연애 상담사 💗스위릐💗")
+
+st.write("나는야 연애고수 스위릐~!!\n")
+st.write("안녕!ㅇ! 나는 너의 연애 고민을 해결해줄 상담사 스위릐야💗💗\n")
+st.write("나에게 너의 연애 고민을 맡겨봐 !ㅇ!")
+st.write("\n\n\n")
+
+st.image(img)
+Prompt = st.text_input("먼저 너의 현재 연애 고민을 알려줘!ㅇ!")
 st.button("click")
 
 result = client.chat.completions.create(
